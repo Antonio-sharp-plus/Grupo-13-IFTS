a01c217783310ba25dc0b32ee79283107b96b67d
Antonio-sharp-plus <antoniozarra8@gmail.com>
2025-05-13 23:15:18 -0300

    Funcion de busqueda populares agregada a api.js

--- public/back/api.js
+++ public/back/api.js
@@ -1,9 +1,10 @@
 const API_TMDB = "4063bb0f4fe2de3c505e5c64207fbc3e";
+const url_popular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_TMDB}&language=es-ES&region=ES`;
 
 async function BuscarPorNombre(nombre) {
   const url_busqueda_nombre = `https://api.themoviedb.org/3/search/movie?api_key=${API_TMDB}&query=${encodeURIComponent(nombre)}&language=es-ES`;
   const response = await fetch(url_busqueda_nombre);
-  if (!response.ok) throw new Error("Error al obtener los datos");
+  if (!response.ok) throw new Error("Error al buscar la película.");
   const peliDatos = await response.json();
   return peliDatos;
 }
@@ -15,4 +16,25 @@ BuscarPorNombre("El padrino")
   .catch(error => console.error("Error:", error));
 
 Código para mostrar el JSON en consola
+*/
+
+async function BuscarPopulares(){
+    try
+    {
+        const response = await fetch(url_popular);
+        const pelisPopulares = await response.json();
+        return pelisPopulares
+    }
+    catch
+    {
+        throw new Error("Error al obtener películas populares.")
+    }
+}
+
+/*
+BuscarPopulares()
+  .then(resultado => console.log(JSON.stringify(resultado, null, 2)))
+  .catch(error => console.error("Error:", error));
+
+Código para mostrar JSON en consola
 */
