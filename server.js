62e21af4dca81002afb183ec1390fbd0ec1ad161
Elian Nahuel <elian.nahuel0348@gmail.com>
2025-05-14 05:36:27 -0300

    Agrego HTML y CSS de Login y Registro con sus respectivas carpetas, también toqué algo en server.js que después habría que revisar

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const path = require('path');
 const PORT = 3000;
 const HOSTNAME = '127.0.0.1';
 
-Server.listen(PORT, HOSTNAME, () => {
+
+const server = http.createServer((req, res) => { //para el routing dice gpt que nos conviene usar express en vez de http, despues lo vemos
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('El servidor se inició correctamente.');
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`El servidor está corriendo en http://${HOSTNAME}:${PORT}/`);
+});
+
+
+/*Server.listen(PORT, HOSTNAME, () => {
     console.log('El servidor está corriendo en http://${HOSTNAME}:${PORT}/')
 });
+Supuestamente esto no va así xd pero después lo vemos
+*/
