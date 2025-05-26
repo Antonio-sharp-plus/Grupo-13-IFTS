d28b2824caad3217ab3cf17f41cecada17c97400
Antonio-sharp-plus <antoniozarra8@gmail.com>
2025-05-26 19:40:29 -0300

    reorganización de archivos backend

--- back/routes/api.general.js
+++ back/routes/api.general.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const apiSeries = require('../public/back/apiSeries');
-const apiPelis = require('../public/back/apiPelis');
+const apiSeries = require('../metodosApi/apiSeries');
+const apiPelis = require('../metodosApi/apiPelis');
 
 
 router.get('/trending', async (req, res) => {
