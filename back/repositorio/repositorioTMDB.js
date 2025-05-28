a4f3df21a7173c81d916d4a26a30e75417074421
Antonio-sharp-plus <antoniozarra8@gmail.com>
2025-05-28 16:17:36 -0300

    Repositorio + Servicio

--- back/repositorio/repositorioTMDB.js
+++ back/repositorio/repositorioTMDB.js
@@ -1,88 +1,94 @@
-const axios = require('axios');
+base_url = 'https://api.themoviedb.org/3';
+api_key = '0794702ad2b0918f600e5733c7ee0dea';
+lenguaje = 'es-ES';
 
-export class repositorioTMDB {
+async function BusquedaGeneral(tipo, query) {
 
-    constructor() {
-        this.api = axios.create({
-            baseURL: "https://api.themoviedb.org/3",
-            params: {
-                api_key: '0794702ad2b0918f600e5733c7ee0dea',
-                lenguaje: 'es-ES'
-            }
-        })
-    }
-
-    async BusquedaGeneral(tipo, query){
-
-        if (tipo === "peliculas"){
-            const response = await this.api.get("/search/movie", {parameters: {
-            query
-        }});
-        } else if (tipo === "series"){
-            const response = await this.api.get("/search/tv", {parameters: {
-            query
-        }});
-        } else{
-            const response = await this.api.get("/search", {parameters: {
-            query
-        }});
-        }
-
+    if (tipo === "peliculas") {
+        const response = await fetch(`${baseURL}/search/movies?query=${query}&api_key=${api_key}&language=${lenguaje}`)
+        return response.data;
+    } else if (tipo === "series") {
+        const response = await fetch(`${baseURL}/search/tv?query=${query}&api_key=${api_key}&language=${lenguaje}`)
+        return response.data;
+    } else {
+        const response = await fetch(`${baseURL}/search/multi?query=${query}&api_key=${api_key}&language=${lenguaje}`)
         return response.data;
     }
 
-    async Trending(){
-        const response = await this.api.get("/trending/all/week");
-        return response.data;
-    }
+}
 
-    async PelisPopulares(){
-        const response = await this.api.get("/movie/popular");
-        return response.data;
-    }
+async function Trending() {
+    const response = await fetch(`${baseURL}/trending/all/week?api_key=${api_key}&language=${lenguaje}`)
+    return response.data;
+}
 
-    async PelisValoradas(){
-        const response = await this.api.get("/movie/top_rated");
-        return response.data;
-    }
+async function PelisPopulares() {
+    const response = await fetch(`${baseURL}/movie/popular?api_key=${api_key}&language=${lenguaje}`)
+    return response.data;
+}
 
-    async PelisEstreno(){
-        const response = await this.api.get("/movie/upcoming");
-        return response.data;
-    }
+async function PelisValoradas() {
+    const response = await fetch(`${baseURL}/movie/top_rated?api_key=${api_key}&language=${lenguaje}`)
+    return response.data;
+}
 
-    async PelisAccion(){
-        const response = await this.api.get("/discover/movie", {params: {with_genres: "28"}});
-        return response.data;
-    }
+async function PelisEstreno() {
+    const response = await fetch(`${baseURL}/movie/upcoming?api_key=${api_key}&language=${lenguaje}`)
+    return response.data;
+}
 
-    async PelisComedia(){
-        const response = await this.api.get("/discover/movie", {params: {with_genres: "35"}});
-        return response.data;
-    }
+async function PelisAccion() {
+    const response = await fetch(`${baseURL}/discover/movie?api_key=${api_key}&language=${lenguaje}&with_genres=28`)
+    return response.data;
+}
 
-     async PelisDrama(){
-        const response = await this.api.get("/discover/movie", {params: {with_genres: "18"}});
-        return response.data;
-    }
+async function PelisComedia() {
+    const response = await fetch(`${baseURL}/discover/movie?api_key=${api_key}&language=${lenguaje}&with_genres=35`)
+    return response.data;
+}
 
-     async PelisSciFi(){
-        const response = await this.api.get("/discover/movie", {params: {with_genres: "878"}});
-        return response.data;
-    }
+async function PelisDrama() {
+    const response = await fetch(`${baseURL}/discover/movie?api_key=${api_key}&language=${lenguaje}&with_genres=18`)
+    return response.data;
+}
 
-    async SeriesPopulares(){
-        const response = await this.api.get("/tv/popular");
-        return response.data;
-    }
+async function PelisSciFi() {
+    const response = await fetch(`${baseURL}/discover/movie?api_key=${api_key}&language=${lenguaje}&with_genres=878`)
+    return response.data;
+}
 
-    async SeriesValoradas(){
-        const response = await this.api.get("/tv/top_rated");
-        return response.data;
-    }
+async function SeriesPopulares() {
+    const response = await fetch(`${baseURL}/tv/popular?api_key=${api_key}&language=${lenguaje}`)
+    return response.data;
+}
 
-    async SeriesComedia(){
-        const response = await this.api.get("/discover/tv");
-        return response.data;
-    }
+async function SeriesValoradas() {
+    const response = await fetch(`${baseURL}/tv/top_rated?api_key=${api_key}&language=${lenguaje}`)
+    return response.data;
+}
+
+async function SeriesComedia() {
+    const response = await fetch(`${baseURL}/discover/tv?api_key=${api_key}&language=${lenguaje}&with_genres=35`)
+    return response.data;
+}
+
+async function SeriesDrama() {
+    const response = await fetch(`${baseURL}/discover/tv?api_key=${api_key}&language=${lenguaje}&with_genres=18`)
+    return response.data;
+}
+
+module.exports = {
+    BusquedaGeneral,
+    Trending,
+    PelisPopulares,
+    PelisValoradas,
+    PelisEstreno,
+    PelisAccion,
+    PelisComedia,
+    PelisDrama,
+    PelisSciFi,
+    SeriesPopulares,
+    SeriesValoradas,
+    SeriesComedia,
+    SeriesDrama
 }
--- back/service/servicioTMDB.js
+++ back/service/servicioTMDB.js
@@ -1,17 +1,186 @@
-const repoTMDB = require("../repositorio/repositorioTMDB");
+const repoTMDB = require('../repositorio/repositorioTMDB')
 
-export class ServicioTMDB{
-
-    async Getmovies(){
-        const datos = repoTMDB.BusquedaGeneral()
-        try{
-            let resultados = datos.results;
-        }
-        catch{
-            console.log("No se obtuvieron los datos de la API");
-            return
-        }
-        
+async function BuscarTodo(nombre) {
+    const datos = repoTMDB.BusquedaGeneral("ambos", nombre);
+    try {
+        let resultados = datos.results;
         return resultados;
     }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarPelículas(nombre) {
+    const datos = repoTMDB.BusquedaGeneral("peliculas", nombre);
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarSeries(nombre) {
+    const datos = repoTMDB.BusquedaGeneral("series", nombre);
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarPelículasPopulares() {
+    const datos = repoTMDB.PelisPopulares();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarPelículasValoradas() {
+    const datos = repoTMDB.PelisValoradas();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarPelículasEstreno() {
+    const datos = repoTMDB.PelisEstreno();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarPelículasAccion() {
+    const datos = repoTMDB.PelisAccion();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarPelículasComedia() {
+    const datos = repoTMDB.PelisComedia();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarPelículasDrama() {
+    const datos = repoTMDB.PelisDrama();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarPelículasSciFi() {
+    const datos = repoTMDB.PelisSciFi();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarSeriesPopulares() {
+    const datos = repoTMDB.SeriesPopulares();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarSeriesValoradas() {
+    const datos = repoTMDB.SeriesValoradas();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarSeriesComedia() {
+    const datos = repoTMDB.SeriesComedia();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+async function BuscarSeriesDrama() {
+    const datos = repoTMDB.SeriesDrama();
+    try {
+        let resultados = datos.results;
+        return resultados;
+    }
+    catch {
+        console.log("No se obtuvieron los datos de la API");
+        return;
+    }
+}
+
+module.exports = {
+    BuscarPelículas,
+    BuscarPelículasAccion,
+    BuscarPelículasComedia,
+    BuscarPelículasDrama,
+    BuscarPelículasEstreno,
+    BuscarPelículasPopulares,
+    BuscarPelículasSciFi,
+    BuscarPelículasValoradas,
+    BuscarSeries,
+    BuscarSeriesComedia,
+    BuscarSeriesDrama,
+    BuscarSeriesPopulares,
+    BuscarSeriesValoradas,
+    BuscarTodo
 }
