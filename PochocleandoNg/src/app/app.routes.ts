83c823f778b36f493244172ddfdbd6f0a2bc8e71
Juan Pablo Schettini <juanpablo.schettini2@gmail.com>
2025-05-28 20:31:56 -0300

    acercade.component
    creación de la sección Acerca de.

--- PochocleandoNg/src/app/app.routes.ts
+++ PochocleandoNg/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home.component';
 import { PeliculasComponent } from './paginas/peliculas/peliculas.component';
 import { SeriesComponent } from './paginas/series/series.component';
+import { AcercadeComponent } from './paginas/acercade/acercade.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // ruta principal
     { path: 'peliculas', component: PeliculasComponent },
-    { path: 'series', component: SeriesComponent }
+    { path: 'series', component: SeriesComponent },
+    { path: 'acercade', component: AcercadeComponent }
 ];
