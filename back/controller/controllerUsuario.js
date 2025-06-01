73305c66abdd6de6a79b062784b2ee996a21ceb4
eliann313 <elian.nahuel0348@gmail.com>
2025-06-01 12:27:58 -0300

    Arreglado el problema con login y registro de la arquitectura de capas

--- back/controller/controllerUsuario.js
+++ back/controller/controllerUsuario.js
@@ -7,7 +7,7 @@ function validarRegistro(body) {
 }
 
 exports.registrarUsuario = async (req, res) => {
-    //console.log('Controller: registrarUsuario', req.body) para controlar erroes
+  //console.log('Controller: registrarUsuario', req.body) //para controlar erroes
   if (!validarRegistro(req.body)) {
     return res.status(400).json({ error: 'Todos los campos son requeridos' });
   }
@@ -15,18 +15,18 @@ exports.registrarUsuario = async (req, res) => {
     await usuarioService.registrarUsuario(req.body);
     res.status(201).json({ mensaje: 'Usuario creado con éxito' });
   } catch (error) {
-    //console.log('Controller registrarUsuario error', error) para controlar errores
+    //console.log('Controller registrarUsuario error', error) //para controlar errores
     res.status(400).json({ error: 'Error al registrar usuario', detalle: error.message });
   }
 };
 
 exports.loginUsuario = async (req, res) => {
-    //console.log('Controller: loginUsuario', req.body) para controlar errores
+    //console.log('Controller: loginUsuario', req.body) //para controlar errores
     try {
     const token = await usuarioService.loginUsuario(req.body);
     res.status(200).json({ mensaje: 'Login exitoso', token });
   } catch (error) {
-    //console.log('Controller loginUsuario error', error) para controlar errores
+    //console.log('Controller loginUsuario error', error) //para controlar errores
     res.status(401).json({ error: error.message });
   }
 };
