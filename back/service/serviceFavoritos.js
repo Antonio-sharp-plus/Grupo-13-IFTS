3099e920356f3f042b64f6017af6ab1b955ce5b2
eliann313 <elian.nahuel0348@gmail.com>
2025-06-13 15:56:46 -0300

    arreglado repo y service de favoritos y actualizaciones en back usuarios

--- back/service/serviceFavoritos.js
+++ back/service/serviceFavoritos.js
@@ -1,9 +1,21 @@
-const repositorioFavoritos = require('../repositorio/repositorioFavoritos');
+const repositorioFavoritos = require("../repositorio/repositorioFavoritos");
 
 exports.agregarFavorito = async (userId, favoritoData) => {
+  try {
+    //console.log("Service: agregarFavorito", userId, favoritoData); //para controlar errores
     await repositorioFavoritos.agregarFavoritoAUsuario(userId, favoritoData);
+  } catch (error) {
+    //console.error("Service agregarFavorito error", error); //para controlar errores
+    return console.log('Error al agregar favorito');
+  }
 };
 
 exports.obtenerFavoritos = async (userId) => {
+  try {
+    //console.log("Service: obtenerFavoritos", userId); //para controlar errores
     return await repositorioFavoritos.obtenerFavoritosDeUsuario(userId);
+  } catch (error) {
+    //console.error("Service obtenerFavoritos error", error); //para controlar errores
+    return console.log('Error al obtener favoritos');
+  }
 };
